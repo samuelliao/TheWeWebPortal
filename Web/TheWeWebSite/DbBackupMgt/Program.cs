640f0495e7f30028121f70caa0effb4b22f4f8f5
Samuel Liao <threesecond2002@gmail.com>
2016-10-26 12:41:16 +0800

    Add project to manage DB backup files. The project can auto delete the expired backup.

---
+++ Web/TheWeWebSite/DbBackupMgt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DbBackupMgt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Directory.Exists(Properties.Settings.Default.FolderPath))
+            {
+                string[] files = Directory.GetFiles(Properties.Settings.Default.FolderPath);
+                DateTime fileModTime;
+                DateTime compareTime = DateTime.Now.AddDays(Properties.Settings.Default.ExpireDay * (-1));
+                foreach(string file in files)
+                {
+                    fileModTime = File.GetLastWriteTime(file);
+                    if(DateTime.Compare(fileModTime, compareTime) < 0)
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+        }
+    }
+}
